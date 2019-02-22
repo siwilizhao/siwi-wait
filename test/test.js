0aa2e5fb0a065cd5216d5dd373d5cff025b7bd59
siwilizhao <siwilizhao@gmail.com>
2019-02-22 22:01:20 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,14 +1,11 @@
-const wait = require('../index')
-
-class Example {
-    constructor() {
-        this.init()
-    }
-    async init () {
-        await wait(10000)
-        console.log(1)
-        return true
-    }
-}
-
-module.exports = new Example()
+const expect = require('chai').expect
+describe('/lib/index.js', () => {
+    it('wait', async () => {
+        const wait = require('../index')
+        let a = 1
+        await wait(1000)
+        a = 2
+        console.log
+        expect(a).to.be.equal(2)
+    });
+});
